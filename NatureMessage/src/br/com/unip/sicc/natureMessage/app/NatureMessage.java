4d130be3931e0dda83018a753ff1b5034ffcd326
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-05-04 18:25:54 -0300

    Chat

--- NatureMessage/src/br/com/unip/sicc/natureMessage/app/NatureMessage.java
+++ NatureMessage/src/br/com/unip/sicc/natureMessage/app/NatureMessage.java
@@ -3,6 +3,7 @@ package br.com.unip.sicc.natureMessage.app;
 import br.com.unip.sicc.natureMessage.viewer.Componentes;
 import br.com.unip.sicc.natureMessage.viewer.TelaCadastro;
 import br.com.unip.sicc.natureMessage.viewer.TelaChat;
+import br.com.unip.sicc.natureMessage.viewer.TelaInicial;
 import br.com.unip.sicc.natureMessage.viewer.TelaLogin;
 import br.com.unip.sicc.natureMessage.viewer.TelaServidor;
 import br.com.unip.sicc.natureMessage.viewer.TelaSobre;
@@ -14,6 +15,6 @@ public class NatureMessage {
         //new TelaChat(5000);
         //new TelaCadastro();
        //new TelaLogin();
-       new TelaLogin();
+       new TelaInicial();
     }
 }
--- NatureMessage/src/br/com/unip/sicc/natureMessage/viewer/TelaInicial.java
+++ NatureMessage/src/br/com/unip/sicc/natureMessage/viewer/TelaInicial.java
@@ -1,29 +1,18 @@
 package br.com.unip.sicc.natureMessage.viewer;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 
 public class TelaInicial {
 
     private PainelPadrao pnlInicial = new PainelPadrao();
-    private Componentes botoesPadrao = new Componentes();
+    private Componentes componentes = new Componentes();
     private JFrame telaInicial = new JFrame();
-    private JLabel lblLogo;
-    private JButton btnInicia;
-    private JLabel lblTitulo;
-    private JLabel lblSubTitulo;
-    private JButton btnManual;
-    private JSeparator lnsManual;
-    private ImageIcon imgCompartilhar = new ImageIcon(getClass().getResource("/br/com/unip/sicc/natureMessage/image/logo.png"));
-    private JLabel lblCompartilhar = new JLabel(imgCompartilhar);
 
     public TelaInicial() {
         telaInicial.setSize(900, 500);
@@ -34,28 +23,17 @@ public class TelaInicial {
     }
 
     public JPanel montaPainelInicial() {
-        
-        lblLogo = new JLabel();
-        lblLogo.setBounds(340, 120, 250, 250);
-        lblLogo.setBorder(null);
-        lblLogo.setBackground(null);
-        lblLogo.setForeground(null);
-        lblLogo.setIcon(imgCompartilhar);
 
-        lblTitulo = new JLabel("NATURE");
-        lblTitulo.setForeground(Color.WHITE);
-        lblTitulo.setFont(new Font("Arial", Font.BOLD, 35));
-        lblTitulo.setBounds(378, 100, 350, 32);
+        JLabel lblLogo = Componentes.lblTexto("", 35, Color.WHITE, 340, 120, 250, 250);
+        lblLogo.setIcon(componentes.image("/br/com/unip/sicc/natureMessage/image/logo.png"));
 
-        lblSubTitulo = new JLabel("MESSAGE");
-        lblSubTitulo.setForeground(Color.WHITE);
-        lblSubTitulo.setFont(new Font("Arial", Font.BOLD, 22));
-        lblSubTitulo.setBounds(396, 124, 350, 32);
+        pnlInicial.add(Componentes.lblTexto("NATURE", 35, Color.WHITE, 378, 100, 350, 32));
+        pnlInicial.add(Componentes.lblTexto("MESSAGE", 22, Color.WHITE, 396, 124, 350, 32));
+        pnlInicial.add(Componentes.linhaSeparadora(352, 380, 200, 1));
 
-        btnInicia = botoesPadrao.montaBtnAlteravel("Iniciar");
+        JButton btnInicia = componentes.montaBtnAlteravel("Iniciar");
         btnInicia.setBounds(277, 342, 350, 30);
         btnInicia.setBackground(new Color(0, 255, 127));
-        btnInicia.setForeground(Color.WHITE);
         btnInicia.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -63,25 +41,18 @@ public class TelaInicial {
                 new TelaLogin();
             }
         });
-        
-        btnManual = botoesPadrao.montaBtnPadrao("Manual de funcionamento");
+
+        JButton btnManual = Componentes.montaBtnPadrao("Manual de funcionamento");
         btnManual.setBounds(378, 377, 150, 35);
-        btnManual.setContentAreaFilled(false);
         btnManual.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Acao invoca manual.
             }
         });
-        
-        lnsManual = new JSeparator();
-        lnsManual.setForeground(Color.WHITE);
-        lnsManual.setBounds(352, 380, 200, 1);
 
         pnlInicial.add(btnInicia);
-        pnlInicial.add(lblTitulo);
-        pnlInicial.add(lblSubTitulo);
         pnlInicial.add(btnManual);
-        pnlInicial.add(lnsManual);
         pnlInicial.add(lblLogo);
 
         return pnlInicial;
