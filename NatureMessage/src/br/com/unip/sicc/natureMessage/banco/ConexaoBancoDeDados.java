7984ec33a9ed6a97974c5fe5a29cf87d4a75557c
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-05-05 16:38:54 -0300

    chat

--- NatureMessage/src/br/com/unip/sicc/natureMessage/banco/ConexaoBancoDeDados.java
+++ NatureMessage/src/br/com/unip/sicc/natureMessage/banco/ConexaoBancoDeDados.java
@@ -1,28 +1,23 @@
 package br.com.unip.sicc.natureMessage.banco;
 
-import br.com.unip.sicc.natureMessage.viewer.Componentes;
 import br.com.unip.sicc.natureMessage.viewer.TelaConexao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class ConexaoBancoDeDados {
-    
+
     public static Connection conexao() {
         try {
             Connection conect = null;
             final String url = "jdbc:mysql://localhost:3306/teste?useTimeZone=true&serverTimezone=UTC";
-            final String drive = "com.mysql.jdbc.Driver";
-            final String noLoginBanco = TelaConexao.usuarioBD;
-            final String noSenhaBanco = TelaConexao.senhaBD;
-            Class.forName(drive);
+            //final String noLoginBanco = TelaConexao.usuarioBD;
+            //final String noSenhaBanco = TelaConexao.senhaBD;
+            final String noLoginBanco = "root";
+            final String noSenhaBanco = "";
             return conect = DriverManager.getConnection(url, noLoginBanco, noSenhaBanco);
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ConexaoBancoDeDados.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
 
