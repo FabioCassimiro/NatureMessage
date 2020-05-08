b848b41086c5ddc1ae850270cdd20b007c686581
Walisoull <62217207+Walisoull@users.noreply.github.com>
2020-05-07 21:58:21 -0300

    feito novo método, preenche sobrenome

--- NatureMessage/src/br/com/unip/sicc/natureMessage/viewer/TelaBiografia.java
+++ NatureMessage/src/br/com/unip/sicc/natureMessage/viewer/TelaBiografia.java
@@ -9,7 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TelaBiografia {
-    
+
     private PainelPadrao pnlBiografia = new PainelPadrao();
     private Componentes componentes = new Componentes();
     private JFrame telaBiografia = new JFrame();
@@ -23,10 +23,84 @@ public class TelaBiografia {
     }
 
     public JPanel montaBiografiaAluno(String aluno) {
-        
-        String sobrenome = null;
+
+        JLabel imgAluno = new JLabel();
+        imgAluno.setIcon(componentes.buscaImagem(aluno + ".jpeg"));
+        imgAluno.setBounds(50, 50, 90, 125);
+
+        JButton btnVoltar = Componentes.montaBotaoIcone(componentes.buscaImagem("setaVoltar.png"), 15, 10, 25, 25, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                telaBiografia.dispose();
+            }
+        });
+
+        pnlBiografia.add(Componentes.montaSeparadora(150, 75, 350, 1));
+        pnlBiografia.add(Componentes.montaTexto(aluno.toUpperCase() + montaSobrenome(aluno).toUpperCase(), 20, Color.WHITE, 150, 10, 500, 100));
         
         switch (aluno) {
+            case "Daniel":         
+                pnlBiografia.add(Componentes.montaTexto(montaFrase(aluno), 15, Color.WHITE, 150, 80, 600, 300));
+                pnlBiografia.add(Componentes.montaTexto("<html>Responsável por:<br> "
+                        + "Desenvolvimento da documentação</html>", 15, Color.WHITE, 150, 30, 400, 150));
+                break;
+            case "Walisson":
+                pnlBiografia.add(Componentes.montaTexto(montaFrase(aluno), 15, Color.WHITE, 150, 95, 600, 300));
+                pnlBiografia.add(Componentes.montaTexto("<html>Responsável por:<br> "
+                        + "Desenvolvimento do software, front-end/design</html>", 15, Color.WHITE, 150, 30, 400, 150));
+                break;
+            case "Fabio":
+                pnlBiografia.add(Componentes.montaTexto(montaFrase(aluno), 15, Color.WHITE, 150, 80, 600, 300));
+                pnlBiografia.add(Componentes.montaTexto("<html>Responsável por:<br> "
+                        + "Desenvolvimento do software, front-end/back-end</html>", 15, Color.WHITE, 150, 30, 400, 150));
+                break;
+        }
+
+        //singleton - somente uma instancia
+        
+        pnlBiografia.add(imgAluno);
+        pnlBiografia.add(btnVoltar);
+
+        return pnlBiografia;
+    }
+
+    public static String montaFrase(String nome) {
+
+        String frase = null;
+
+        switch (nome) {
+            case "Fabio":
+                frase = "<html></html>";
+                break;
+            case "Walisson":
+                frase = "<html>Meu nome é Walisson, tenho 19 anos, e sou estudante"
+                        + " de Ciência da Computação. Atualmente trabalho na Ericsson, atuando como"
+                        + " desenvolvedor de testes (QA - Quality Assurance).<br><br>"
+                        + "\"Diante da vastidão do tempo e da imensidão do universo, é um imenso prazer para mim dividir um planeta e uma época com vocês.\"<br>"
+                        + "Sagan, Carl 05/2020</html>";
+                break;
+            case "Daniel":
+                frase = "<html>Meu nome é Daniel, tenho 20 anos, e sou estudante"
+                        + " de Ciência da Computação. Atualmente trabalho na Ericsson, atuando como"
+                        + " desenvolvedor RPA.<br><br>"
+                        + "\"Quanto mais certeza temos de quem somos, menos importa o que pensam sobre quem a gente é.\"   <br>"
+                        + ".&emsp; 05/2020</html>";
+                break;
+            case "Joao":
+                frase = "algo";
+                break;
+            case "Marcos":
+                frase = "algo";
+                break;
+        }
+        return frase;
+    }
+
+    public static String montaSobrenome(String nome) {
+
+        String sobrenome = null;
+
+        switch (nome) {
             case "Fabio":
                 sobrenome = " Cassimiro";
                 break;
@@ -45,51 +119,7 @@ public class TelaBiografia {
             default:
                 break;
         }
-        
-       
-        JLabel imgAluno = new JLabel();
-        imgAluno.setIcon(componentes.buscaImagem(aluno + ".jpeg"));
-        imgAluno.setBounds(50, 50, 90, 125);
-        
 
-        JButton btnVoltar = Componentes.montaBotaoIcone(componentes.buscaImagem("setaVoltar.png"), 15, 10, 25, 25, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                telaBiografia.dispose();
-            }
-        });
-        
-        pnlBiografia.add(Componentes.montaTexto(aluno.toUpperCase() + sobrenome.toUpperCase(), 20, Color.WHITE, 150, 15, 500, 100));
-        pnlBiografia.add(Componentes.montaTexto(montaFrase(aluno), 15, Color.WHITE, 150, 178, 100, 100));
-        pnlBiografia.add(imgAluno);
-        pnlBiografia.add(btnVoltar);
-        
-        return pnlBiografia;
-    }
-
-    public static String montaFrase(String nome) {
-        
-        String frase = null;
-        
-        switch (nome) {
-            case "Fabio":
-                frase = "<html></html>";
-                //não coube
-                break;
-            case "Walisson":
-                frase = "algo";
-                break;
-            case "Daniel":
-                frase = "<html>Meu nome é Daniel, tenho 20 anos, e sou estudante"
-                        + "de Ciência da Computação. Atualmente trabalho na Ericsson</html>";
-                break;
-            case "Joao":
-                frase = "algo";
-                break;
-            case "Marcos":
-                frase = "algo";
-                break;
-        }
-        return frase;
+        return sobrenome;
     }
 }
